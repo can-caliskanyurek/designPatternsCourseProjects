8fa97281f5a47dba1f93914549d0023f391875f4
Can Çalışkanyürek <can.caliskanyurek@gmail.com>
2022-03-02 11:40:46 +0300

    bridge pattern düzenleme

--- StructuralPatterns/StructuralPatterns.Bridge/BridgeCodingExercise/Interfaces/IRenderer.cs
+++ StructuralPatterns/StructuralPatterns.Bridge/BridgeCodingExercise/Interfaces/IRenderer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BridgeCodingExercise.Interfaces
+﻿namespace BridgeCodingExercise.Interfaces
 {
     public interface IRenderer
     {
